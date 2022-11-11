be5e4fefe5060b8f30eb375a9abedd558a94d5be
Greg Bowler <greg.bowler@g105b.com>
2022-11-11 10:03:34 +0000

    wip: test where deprecations are emitted

--- test/behat/features/bootstrap/FeatureContext.php
+++ test/behat/features/bootstrap/FeatureContext.php
@@ -1,9 +1,7 @@
 <?php
 use Behat\MinkExtension\Context\MinkContext;
 
+die("This is a debug line to detect where deprecations are emitted");
+
 class FeatureContext extends MinkContext {
-	/** @beforeSuite */
-	public static function setup() {
-		die("There should be no deprecations at this point");
-	}
 }
